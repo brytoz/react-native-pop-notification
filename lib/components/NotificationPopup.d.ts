7cadc91239395e0dbd57fccd0f433bbc435e8c88
MAC <brytozee@yahoo.com>
2025-04-01 00:58:04 +0100

    ios

--- lib/components/NotificationPopup.d.ts
+++
@@ -1,3 +0,0 @@
-declare const NotificationPopup: () => import("react").JSX.Element | null;
-export default NotificationPopup;
-//# sourceMappingURL=NotificationPopup.d.ts.map
--- lib/services/LocalNotificationManager.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const triggerLocalNotification: (notification: any) => void;
-export declare const onLocalNotification: (callback: (data: any) => void) => void;
-//# sourceMappingURL=LocalNotificationManager.d.ts.map
